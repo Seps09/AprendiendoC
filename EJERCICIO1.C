ec0dd2b11b6d4a610bcdce665ef59fd63b823d5f
JesusYndriago <yndriagojesus99@gmail.com>
2026-04-30 10:47:15 -0500

    commit Explicación del ejercicio

--- EJERCICIO1.C
+++ EJERCICIO1.C
@@ -3,10 +3,16 @@
 
 void main(void){
 
-    printf("Hola Mundo");
-
-    print("Hola mundo en linea 8");
-
-    printf("Hola mundo desde linea 10");
+    /**
+     * Realizar una calculadora que reciba 2 parametros por teclado y
+     * Sume.
+     * Reste.
+     * Multiplique.
+     * Divida.
+     * Obtenga la potencia.
+     * Obtenga el cociente.
+     * indique si es mayor igual, o menor.
+     * Que imprima los numeros desde el primer parametro al segundo parametro.
+     */
 
 }
